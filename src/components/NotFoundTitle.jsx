fdc1809e84a17f406809eca6556ba2402a67cdac
appitsolution <applicationitsolution@gmail.com>
2023-04-26 21:03:45 +0300

    commit added home

--- src/components/NotFoundTitle.jsx
+++ src/components/NotFoundTitle.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import OnlyMobile from "../components/OnlyMobile";
 
 const NotFoundTitle = () => {
   return (
-    <div className="not-found">
-      <h1 className="not-found-title">404</h1>
-      <h2 className="not-found-second">Page Not Found</h2>
-    </div>
+    <>
+      <div className="not-found">
+        <h1 className="not-found-title">404</h1>
+        <h2 className="not-found-second">Page Not Found</h2>
+      </div>
+      <OnlyMobile />
+    </>
   );
 };
 
